da68433b80b12644bd0d7356b7b447e0caebbdf6
Mehdi <mehdi.hesenli.2006@gmail.com>
2025-12-12 12:06:11 +0400

    Fixed health

--- app/app.py
+++ app/app.py
@@ -83,7 +83,7 @@ async def request_timing_middleware(request, call_next):
 
 
 
-@app.get("/health", response_model=HealthResponse)
+@app.head("/health", response_model=HealthResponse)
 async def health_check() -> HealthResponse:
     return HealthResponse(status="ok")
 
