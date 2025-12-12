f758da4243a1d93a00a7806bab1b676ca149f2b4
Mehdi <mehdi.hesenli.2006@gmail.com>
2025-12-12 13:40:39 +0400

    Fixed Trivia

--- app/app.py
+++ app/app.py
@@ -84,6 +84,9 @@ async def request_timing_middleware(request, call_next):
 
 
 
+@app.get("/health", response_model=HealthResponse)
+async def health_check() -> HealthResponse:
+    return HealthResponse(status="ok")
 @app.head("/health", response_model=HealthResponse)
 async def health_check() -> HealthResponse:
     return HealthResponse(status="ok")
